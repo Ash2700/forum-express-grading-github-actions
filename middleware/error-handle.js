4bf48fe9ef32d838135a2880c0af581d46a090b5
Ash2700 <vmvuruvu@gmail.com>
2024-02-02 12:31:16 +0800

    flash msg& signup verification

---
+++ middleware/error-handle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  generalErrorHandler (error, req, res, next) {
+    if (error instanceof Error) {
+      req.flash('error_messages', `${error.name}:${error.message}`)
+    } else {
+      req.flash('error_messages', `${error}`)
+    }
+    res.redirect('back')
+    next(error)
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handle')
 
 router.use('/admin', admin)
 router.get('/signup', userController.singUpPage)
 router.post('/signup', userController.singUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', (req, res) => res.redirect('/signup'))
+router.use('/', generalErrorHandler)
 module.exports = router
