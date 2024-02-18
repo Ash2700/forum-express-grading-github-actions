ecc541e551c025c51e31071f7728f9cd566101d7
Ash2700 <vmvuruvu@gmail.com>
2024-02-18 11:00:31 +0800

    add ifCond hbs helper &update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,5 +3,8 @@ module.exports = {
   currentYear: () => dayjs().year(),
   displayRole: function (a, option) {
     return a === 1 ? option.fn(this) : option.inverse(this)
+  },
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
   }
 }
